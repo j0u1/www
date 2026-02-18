41af8eb172194e51db93e984aa92eeaa47fa0b57
j0u1 <158160207+j0u1@users.noreply.github.com>
2026-02-19 01:39:51 +0300

    chore(LogoAndAvatar): change image size

--- app/components/Main/LogoAndAvatar.tsx
+++ app/components/Main/LogoAndAvatar.tsx
@@ -10,7 +10,7 @@ export default function LogoAndAvatar() {
   return (
     <div onClick={() => setChangeLogo(!changeLogo)} className="size-14 cursor-pointer">
       <Logo className={`size-14 hover:scale-105 duration-300 transition-all ${changeLogo ? 'translate-y-0' : '-translate-y-12 opacity-0 pointer-events-none'}`} />
-      <Image className={`size-14 hover:scale-105 duration-300 transition-all ${!changeLogo ? '-translate-y-14' : 'translate-y-12 opacity-0 pointer-events-none'}`} src="https://avatar.spoverlay.ru/face/d1yy?w=52" alt="мой minecraft скин" width={50} height={50} />
+      <Image className={`size-14 hover:scale-105 duration-300 transition-all ${!changeLogo ? '-translate-y-14' : 'translate-y-12 opacity-0 pointer-events-none'}`} src="https://avatar.spoverlay.ru/face/d1yy?w=128" alt="мой minecraft скин" width={56} height={56} />
     </div>
   )
 }
--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [new URL('https://avatar.spoverlay.ru/face/d1yy?w=52')],
+    remotePatterns: [new URL('https://avatar.spoverlay.ru/face/d1yy?w=128')],
   },
 };
 
