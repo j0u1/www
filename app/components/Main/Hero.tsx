2ab514931ed8372fcea4715cec1fe331caa47c38
j0u1 <158160207+j0u1@users.noreply.github.com>
2026-02-25 23:20:34 +0300

    fix(Hero): reorder class names for improved readability and consistency

--- app/components/Main/Hero.tsx
+++ app/components/Main/Hero.tsx
@@ -6,7 +6,7 @@ import Skills from "./Skills";
 export default function Hero() {
   return (
     // lg:h-[85dvh]
-    <section className="flex relative justify-center items-center min-h-[97dvh] lg:min-h-162.5 lg:h-[94dvh] pb-36 md:pb-30 xl:pb-22 duration-500 transition-all">
+    <section className="flex relative justify-center items-center pb-36 transition-all duration-500 min-h-[97dvh] md:pb-30 lg:min-h-162.5 lg:h-[94dvh] xl:pb-22">
       <Links />
       <H1 className="flex gap-4 items-center group">
         <Cross />
@@ -20,7 +20,7 @@ export default function Hero() {
 
 function Cross() {
   return (
-    <p className="flex gap-0.5 select-none font-normal text-additional/70" aria-hidden>
+    <p className="flex gap-0.5 font-normal select-none text-additional/70" aria-hidden>
       <span className="transition-transform duration-500 group-hover:rotate-45">+</span>
       <span className="transition-transform duration-500 group-hover:rotate-45">+</span>
     </p>
