2a491915542962302f26a88def538457ecb3c90b
j0u1 <158160207+j0u1@users.noreply.github.com>
2026-02-25 23:28:47 +0300

    fix(Projects): update project descriptions and titles for clarity and consistency

--- app/data/projects.ts
+++ app/data/projects.ts
@@ -1,41 +1,42 @@
 export const projects = [
   {
     title: "MJITIOOO",
-    description: "Расширение для Twitch, добавляющее поддержку пользовательских эмодзи и улучшенное управление ими.",
+    description: "Личный сайт MJITIOOO",
     image: "/images/projects/mjitiooo.png",
     link: "https://mjitiooo.d1yy.ru",
   },
   {
-    title: "SP Pack",
-    description: "Расширение для Twitch, добавляющее поддержку пользовательских эмодзи и улучшенное управление ими.",
+    title: "SP Pack (spoverlay)",
+    description: "Сайт для скачивания общего ресурс пака СП5",
     image: "/images/projects/sppack.png",
     link: "https://pack.spoverlay.ru",
   },
   {
-    title: "Портал Услуг",
-    description: "Расширение для Twitch, добавляющее поддержку пользовательских эмодзи и улучшенное управление ими.",
+    title: "Портал Услуг (spoverlay)",
+    description: "Инструмент для МЮ СП5 редактированием и созданием территорий и паспортов",
     image: "/images/projects/portal.png",
   },
   {
     title: "PawVille Community",
-    description: "Расширение для Twitch, добавляющее поддержку пользовательских эмодзи и улучшенное управление ими.",
+    description: "Сайт для комьюнити ЛапВиль",
     image: "/images/projects/pawville.png",
     link: "https://pawville.community",
     position: "bottom",
   },
   {
     title: "Spilya",
-    description: "Расширение для Twitch, добавляющее поддержку пользовательских эмодзи и улучшенное управление ими.",
+    description: "Личный сайт Spilya",
     image: "/images/projects/spilya.png",
   },
   {
-    title: "Gallery",
-    description: "Расширение для Twitch, добавляющее поддержку пользовательских эмодзи и улучшенное управление ими.",
+    title: "Gallery (spoverlay)",
+    description: "Галерея артов СП5, в которой собраны некоторые арты сервера.",
     image: "/images/projects/gallery.png",
   },
   {
-    title: "SPEMOTES",
-    description: "Расширение для Twitch, добавляющее поддержку пользовательских эмодзи и улучшенное управление ими.",
+    title: "SPEMOTES (spoverlay)",
+    description: "Сайт с более удобной загрузкой SPEMOTES для мода EmoteCraft",
     image: "/images/projects/spemotes.png",
+    link: "https://spemotes.spoverlay.ru/",
   },
 ]
