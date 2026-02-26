1eae0b1684f39090147cd08c26c5cc01838ca9b5
j0u1 <158160207+j0u1@users.noreply.github.com>
2026-02-26 17:15:41 +0300

    style: change footer text color

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <>
       <Hero />
       <Projects />
-      <footer className="text-neutral-300">
+      <footer className="text-additional">
         <p className="lg:max-w-3xl text-sm md:text-base">
           *<span className="text-accent">spoverlay</span> — команда разработчиков сайтов для серверов СП. Я основал её, придумал большинство идей, в основном занимаюсь дизайном, вёрсткой и фронтендом 
         </p>
