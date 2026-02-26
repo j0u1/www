efa5a36e20906e3b3829db503aee3134e6c0be31
j0u1 <158160207+j0u1@users.noreply.github.com>
2026-02-26 16:39:14 +0300

    chore: add * to spoverlay's projects

--- app/data/projects.ts
+++ app/data/projects.ts
@@ -6,13 +6,13 @@ export const projects = [
     link: "https://mjitiooo.d1yy.ru",
   },
   {
-    title: "SP Pack (spoverlay)",
+    title: "SP Pack (spoverlay*)",
     description: "Сайт для скачивания общего ресурс пака СП5",
     image: "/images/projects/sppack.png",
     link: "https://pack.spoverlay.ru",
   },
   {
-    title: "Портал Услуг (spoverlay)",
+    title: "Портал Услуг (spoverlay*)",
     description: "Инструмент для МЮ СП5 редактированием и созданием территорий и паспортов",
     image: "/images/projects/portal.png",
   },
@@ -29,12 +29,12 @@ export const projects = [
     image: "/images/projects/spilya.png",
   },
   {
-    title: "Gallery (spoverlay)",
+    title: "Gallery (spoverlay*)",
     description: "Галерея артов СП5, в которой собраны некоторые арты сервера.",
     image: "/images/projects/gallery.png",
   },
   {
-    title: "SPEMOTES (spoverlay)",
+    title: "SPEMOTES (spoverlay*)",
     description: "Сайт с более удобной загрузкой SPEMOTES для мода EmoteCraft",
     image: "/images/projects/spemotes.png",
     link: "https://spemotes.spoverlay.ru/",
