6711f6dbc943ad66af0448fd1e23dea4c3439ed1
j0u1 <158160207+j0u1@users.noreply.github.com>
2026-03-04 01:56:34 +0300

    fix(avatar): replace remote image with local

--- app/components/Main/LogoAndAvatar.tsx
+++ app/components/Main/LogoAndAvatar.tsx
@@ -12,7 +12,7 @@ export default function LogoAndAvatar() {
   return (
     <div onClick={() => setChangeLogo(!changeLogo)} className={`${sizeClass} cursor-pointer`}>
       <Logo className={`${logoClasses} ${changeLogo ? 'translate-y-0' : '-translate-y-12 opacity-0 pointer-events-none'}`} />
-      <Image className={`${logoClasses} ${!changeLogo ? '-translate-y-8 md:-translate-y-10 lg:-translate-y-14' : 'translate-y-12 opacity-0 pointer-events-none'}`} src="https://avatar.spoverlay.ru/face/d1yy?w=128" alt="мой minecraft скин" width={56} height={56} />
+      <Image className={`${logoClasses} ${!changeLogo ? '-translate-y-8 md:-translate-y-10 lg:-translate-y-14' : 'translate-y-12 opacity-0 pointer-events-none'}`} src="/images/avatar-mc.png" alt="мой minecraft скин" width={56} height={56} />
     </div>
   )
 }
--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [new URL('https://avatar.spoverlay.ru/face/d1yy?w=128')],
-  },
 };
 
 export default nextConfig;
+
+
+new Promise((resolve, reject) => {
+  setTimeout(() => {
+    const data = { id: 1, name: "123" }
+
+    resolve(data)
+  }, 1000)
+})
