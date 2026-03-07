2ecb9e8c251dd576411f60480e36af2bbb9f40b1
j0u1 <158160207+j0u1@users.noreply.github.com>
2026-03-07 19:55:48 +0300

    chore: add new projects

--- app/data/projects.ts
+++ app/data/projects.ts
@@ -1,5 +1,18 @@
 export const projects = [
   {
+    title: "StarHub",
+    description:
+      "Платформа для просмотра звёзд конкретного пользователя на GitHub",
+    image: "/images/projects/starhub.png",
+    link: "https://starhub.d1yy.ru",
+  },
+  {
+    title: "yat",
+    description: "yet another todo (ещё один todo-лист)",
+    image: "/images/projects/yat.png",
+    link: "https://yat.d1yy.ru",
+  },
+  {
     title: "MJITIOOO",
     description: "Личный сайт MJITIOOO",
     image: "/images/projects/mjitiooo.png",
@@ -13,7 +26,8 @@ export const projects = [
   },
   {
     title: "Портал Услуг (spoverlay*)",
-    description: "Инструмент для МЮ СП5 редактированием и созданием территорий и паспортов",
+    description:
+      "Инструмент для МЮ СП5 редактированием и созданием территорий и паспортов",
     image: "/images/projects/portal.png",
   },
   {
@@ -39,4 +53,4 @@ export const projects = [
     image: "/images/projects/spemotes.png",
     link: "https://spemotes.spoverlay.ru/",
   },
-]
+];
