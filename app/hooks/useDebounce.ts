04f57d6e7553a7cda8abeb476e1aeab3e5a79bc4
j0u1 <158160207+j0u1@users.noreply.github.com>
2026-03-12 21:58:02 +0300

    chore(useDebounce): remove commented code

--- app/hooks/useDebounce.ts
+++ app/hooks/useDebounce.ts
@@ -1,22 +1,6 @@
 "use client"
 import { useEffect, useState } from "react";
 
-// export function useDebounce(value:string, delay = 300) {
-//   const [debounceValue, setDebounceValue] = useState(value)
-
-//   useEffect(() => {
-//     const handler = setTimeout(() => {
-//       setDebounceValue(value)
-//     }, delay)
-
-//     return () => {
-//       clearTimeout(handler)
-//     }
-//   }, [value, delay])
-
-//   return debounceValue
-// }
-
 export function useDebounce(value:string, delay = 300) {
   const [debaunceValue, setDebaunceValue] = useState(value)
 
