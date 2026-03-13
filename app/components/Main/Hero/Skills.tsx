376c78e3b3f6b71cd7c9634e73c648b9f7600d05
j0u1 <158160207+j0u1@users.noreply.github.com>
2026-03-13 07:43:07 +0300

    style(Hero): adjust gap on mobile

--- app/components/Main/Hero/Skills.tsx
+++ app/components/Main/Hero/Skills.tsx
@@ -6,7 +6,7 @@ export default function Skills() {
   const iconSizes = "hover:scale-110 size-5 md:size-6 lg:size-8 duration-300 transition-all"
 
   return (
-    <section className="flex absolute right-0 bottom-0 left-0 flex-col gap-4 justify-between w-full transition-all duration-300 lg:flex-row lg:gap-0">
+    <section className="flex absolute right-0 bottom-0 left-0 flex-col gap-6 justify-between w-full transition-all duration-300 lg:flex-row lg:gap-0">
       {SKILLS.map((skill, index) => (
         <div key={index} className={`relative flex flex-col gap-6 ${skill.right ? 'text-right group/uxui' : 'text-left group/frontend'}`}>
           <H2 className={`relative z-10 leading-none flex items-center gap-3.5 ${skill.right ? 'justify-end' : 'justify-start'}`}>
