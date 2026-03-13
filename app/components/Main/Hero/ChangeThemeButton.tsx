7cc63ff83c1b0ffab8ff0d3f035947c02dcb0457
j0u1 <158160207+j0u1@users.noreply.github.com>
2026-03-13 07:40:54 +0300

    fix: correct theme icon behavior

---
+++ app/components/Main/Hero/ChangeThemeButton.tsx
@@ -0,0 +1,18 @@
+type ThemeButtonProps = {
+  toggle: () => void
+}
+
+export default function ChangeThemeButton({ toggle }: ThemeButtonProps) {
+  return (
+    <div onClick={toggle} className="relative cursor-pointer">
+      <div className="absolute rounded-full transition-all duration-300 size-5.5 bg-additional" />
+      <div
+        className="relative size-5.5 duration-300 transition-all -translate-y-1.5 translate-x-1.5 dark:translate-y-0 dark:translate-x-0"
+      >
+        <div
+          className="absolute size-5.5 rounded-full duration-300 transition-all bg-background dark:bg-additional"
+        />
+      </div>
+    </div>
+  )
+}
