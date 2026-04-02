c65fcb633c197a34be2d47ffb1a85d4790d68a2c
j0u1 <158160207+j0u1@users.noreply.github.com>
2026-04-02 22:44:44 +0300

    style(Hero): fix Frontend capitalization

--- app/data/skillsText.ts
+++ app/data/skillsText.ts
@@ -1,25 +1,25 @@
-import FigmaLogo from "../components/icons/Logos/FigmaLogo";
-import TSLogo from "../components/icons/Logos/TSLogo";
-import { Skills } from "../types/skills";
+import FigmaLogo from '../components/icons/Logos/FigmaLogo';
+import TSLogo from '../components/icons/Logos/TSLogo';
+import { Skills } from '../types/skills';
 
 export const SKILLS: Skills[] = [
   {
     icon: TSLogo,
-    title: "FrontEnd-разработчик",
+    title: 'Frontend-разработчик',
     description: [
-      "Делаю интерфейсы, которые выглядят просто и адаптивно работают.",
-      "Основной стек — TypeScript, Nuxt (Vue), TailwindCSS.",
-      "Владею также React, JavaScript, Sass, Astro, Python, Vite, SQL"
-    ]
+      'Делаю интерфейсы, которые выглядят просто и адаптивно работают.',
+      'Основной стек — TypeScript, Nuxt (Vue), TailwindCSS.',
+      'Владею также React, JavaScript, Sass, Astro, Python, Vite, SQL',
+    ],
   },
   {
     icon: FigmaLogo,
-    title: "UI/UX-дизайнер",
+    title: 'UI/UX-дизайнер',
     description: [
-      "Проектирую интерфейсы, которые остаются удобными в работе.",
-      "Фокус — функциональность, ясность и удобство.",
-      "Владею Figma, Adobe Photoshop и Adobe Illustrator"
+      'Проектирую интерфейсы, которые остаются удобными в работе.',
+      'Фокус — функциональность, ясность и удобство.',
+      'Владею Figma, Adobe Photoshop и Adobe Illustrator',
     ],
-    right: true
-  }
-] as const
+    right: true,
+  },
+] as const;
