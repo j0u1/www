401ebfa5a690ee1e94c5d2972170007b807d7b71
j0u1 <158160207+j0u1@users.noreply.github.com>
2026-06-13 14:12:45 +0300

    style(NotFound): increase icon size and apply foreground color variable

--- app/components/Main/Projects/NotFound.tsx
+++ app/components/Main/Projects/NotFound.tsx
@@ -1,7 +1,7 @@
 export default function NotFound() {
   return (
     <div className="flex justify-center items-center gap-4 text-additional mb-8">
-      <span className="text-2xl">:/</span>
+      <span className="text-3xl text-(--additional-foreground)">:/</span>
       <p className="flex flex-col">
         <span>Такого проекта не существует,</span>
         <span>перепроверьте введёное значение</span>
