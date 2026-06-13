7d88abffd4635e07e1c82a3c978edbdcf3241d3f
j0u1 <158160207+j0u1@users.noreply.github.com>
2026-06-13 14:23:40 +0300

    fix(Skills): update icon size class to use active state for scaling

--- app/components/Main/Hero/Skills.tsx
+++ app/components/Main/Hero/Skills.tsx
@@ -3,7 +3,7 @@ import H2 from "../../UI/Heading/H2";
 import P from "../../UI/Heading/P";
 
 export default function Skills() {
-  const iconSizes = "hover:scale-110 size-5 md:size-6 lg:size-8 duration-300 transition-all"
+  const iconSizes = "active:scale-110 size-5 md:size-6 lg:size-8 duration-300 transition-all"
 
   return (
     <section className="flex absolute right-0 bottom-0 left-0 flex-col gap-6 justify-between w-full transition-all duration-300 lg:flex-row lg:gap-0">
