1b95bcb944164c0b079852b0e4c5a1d97c62cfbe
j0u1 <158160207+j0u1@users.noreply.github.com>
2026-07-13 13:22:39 +0300

    fix(Footer): format code and improve layout consistency

--- app/components/Main/Footer.tsx
+++ app/components/Main/Footer.tsx
@@ -1,31 +1,35 @@
-import Link from "next/link";
-import QrCode from "../icons/QrCode";
-import Lnk from "../UI/Lnk";
+import QrCode from '../icons/QrCode';
+import Lnk from '../UI/Lnk';
 
 const links = [
   {
-    title: "Проекты",
-    href: "./projects"
+    title: 'Проекты',
+    href: './projects',
   },
   {
-    title: "Скиллы",
-    href: "./skills"
+    title: 'Скиллы',
+    href: './skills',
   },
-]
+];
 
 export default function Footer() {
   return (
-    <footer className="grid grid-cols-3 w-full items-center pt-24 pb-20 border-t border-secondary">
-
-      <Lnk className="text-9xl size-fit" href={"/"}>d1yy</Lnk>
-      <div className="flex items-center gap-6 mx-auto">
+    <footer className="grid grid-cols-2 xl:grid-cols-3 w-full items-center pt-24 pb-20 border-t border-secondary">
+      <Lnk className="text-9xl size-fit" href={'/'}>
+        d1yy
+      </Lnk>
+      <div className="flex items-center gap-6 md:justify-end lg:justify-center md:ml-auto xl:mx-auto">
         {links.map((link, id) => (
-          <Lnk className="text-xl [&+a]:border-l border-additional-foreground pl-6" key={id} href={link.href}>
+          <Lnk
+            className="text-xl [&+a]:border-l border-additional-foreground pl-6"
+            key={id}
+            href={link.href}
+          >
             {link.title}
           </Lnk>
         ))}
       </div>
-      <QrCode className="size-25 ml-auto" />
+      <QrCode className="hidden xl:block size-25 ml-auto" />
     </footer>
-  )
+  );
 }
